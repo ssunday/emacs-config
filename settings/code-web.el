342597b71ac35e2898896633c6d8dfb37bce249c
Sarah Sunday <ssunday@8thlight.com>
2016-12-02 14:08:41 -0600

    Web mode actually is used now

--- settings/code-web.el
+++ settings/code-web.el
@@ -16,7 +16,16 @@
   :ensure t)
 
 (use-package web-mode
-  :ensure t)
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+  (setq web-mode-markup-indent-offset 2))
 
 (use-package company-web
   :ensure t
