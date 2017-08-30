f25a6fc9e1dd02abe5974daf816c73762edf8f42
Sarah Sunday <3252602+ssunday@users.noreply.github.com>
2017-08-30 10:13:08 -0500

    Various misc changes

--- settings/general.el
+++ settings/general.el
@@ -1,11 +1,14 @@
 ;;General Emacs Settings
 
+(setq ring-bell-function 'ignore)
+
 (delete-selection-mode 1)
+
 (fset 'yes-or-no-p 'y-or-n-p)
+
 (setq kill-buffer-query-functions
-  (remq 'process-kill-buffer-query-function
-	kill-buffer-query-functions))
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+      (remq 'process-kill-buffer-query-function
+            kill-buffer-query-functions))
 
 (setq backup-inhibited t
       auto-save-default nil
@@ -13,21 +16,30 @@
       x-select-enable-clipboard t
       confirm-nonexistent-file-or-buffer nil
       mouse-wheel-progressive-speed nil
-      mac-command-key-is-meta nil)
+      mac-command-key-is-meta nil
+      mouse-drag-copy-region nil)
 
 (global-auto-revert-mode)
 
 (global-set-key (kbd "s-/") 'comment-or-uncomment-region)
 
-(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C-=") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
+
+;; Whitespace
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(setq-default indent-tabs-mode nil)
+
+(electric-indent-mode 1)
 
 ;;Buffers
 
 (use-package ibuffer
-    :bind ("C-x C-b" . ibuffer)
-    :config
-    (setq ibuffer-default-sorting-mode 'major-mode))
+  :bind ("C-x C-b" . ibuffer)
+  :config
+  (setq ibuffer-default-sorting-mode 'major-mode))
 
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
