cf410bed7e4e8bb43db198e13078a3eb49615ade
Sarah Sunday <3252602+ssunday@users.noreply.github.com>
2017-11-17 07:59:47 -0600

    Remove automatic rubocop

--- settings/code-ruby.el
+++ settings/code-ruby.el
@@ -8,10 +8,4 @@
 (eval-after-load 'company
   '(push 'company-robe company-backends))
 
-(defun run-rubocop ()
-  (when (eq major-mode 'ruby-mode)
-    (rubocop-autocorrect-current-file)))
-
-(add-hook 'before-save-hook #'run-rubocop)
-
 (provide 'code-ruby)
