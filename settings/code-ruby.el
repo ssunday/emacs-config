05c090316a75fc52b4efd52a1f0da5895bf3ec2d
Sarah Sunday <3252602+ssunday@users.noreply.github.com>
2017-11-17 07:22:04 -0600

    Run rubocop before saving a file

--- settings/code-ruby.el
+++ settings/code-ruby.el
@@ -2,12 +2,16 @@
 (use-package rubocop
   :ensure t)
 
-
 (use-package robe
   :ensure t)
 
-
 (eval-after-load 'company
   '(push 'company-robe company-backends))
+
+(defun run-rubocop ()
+  (when (eq major-mode 'ruby-mode)
+    (rubocop-autocorrect-current-file)))
+
+(add-hook 'before-save-hook #'run-rubocop)
 
 (provide 'code-ruby)
