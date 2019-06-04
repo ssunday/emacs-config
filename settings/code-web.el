3c48d5192ea3e2f088eb273e51d12a04bfe484f5
Sarah Sunday <3252602+ssunday@users.noreply.github.com>
2019-06-04 18:12:50 -0500

    [CODE] Jekyll/html alignment tweaks

--- settings/code-web.el
+++ settings/code-web.el
@@ -30,6 +30,10 @@
   (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.eex\\'" . web-mode))
+
+  (set-face-attribute 'web-mode-block-delimiter-face   nil :foreground "MediumOrchid2")
+  (set-face-attribute 'web-mode-block-control-face     nil :foreground "MediumOrchid2")
+
   (setq web-mode-js-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
@@ -39,6 +43,7 @@
   (setq web-mode-code-indent-offset 2))
 
 (setq web-mode-content-types-alist '(("jsx" . ".*\\.tsx?")))
+(setq web-mode-engines-alist '(("django" . "\\.html\\'")))
 
 (use-package company-web
   :ensure t
