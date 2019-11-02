62ca35cfd2ae7f04db71acf880f9786f32f18f90
Sarah Sunday <3252602+ssunday@users.noreply.github.com>
2019-11-02 09:28:06 -0500

    [EDITOR] Cleanup searching config

--- settings/editor/searching.el
+++ settings/editor/searching.el
@@ -1,25 +1,17 @@
 (use-package visual-regexp
-  :ensure t)
-
-(global-set-key (kbd "s-r") 'vr/replace)
-
-(defun set-exec-path-from-shell-PATH ()
-  (interactive)
-  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-    (setenv "PATH" path-from-shell)
-    (setq exec-path (split-string path-from-shell path-separator))))
+  :ensure t
+  :bind (("s-r" . vr/replace)))
 
 (use-package ag
-  :ensure t)
+  :ensure t
+  :config
+  (setq ag-highlight-search t
+        ag-reuse-window t
+        ag-reuse-buffers t)
+  :bind
+  (("s-F" . ag)))
 
 (use-package dash
   :ensure t)
 
-(global-set-key (kbd "s-F") 'ag)
-
-(set-exec-path-from-shell-PATH)
-(setq ag-highlight-search t
-      ag-reuse-window t
-      ag-reuse-buffers t)
-      
 (provide 'editor/searching)
