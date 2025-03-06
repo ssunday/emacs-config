ff29ef0a2dbba7ebaf642486d6f5087c6b7c7407
Sarah Sunday <3252602+ssunday@users.noreply.github.com>
2025-03-06 08:11:19 -0600

    [CODE] Remove company-ghc

--- settings/code/haskell.el
+++ settings/code/haskell.el
@@ -3,11 +3,4 @@
 (use-package haskell-mode
   :ensure t)
 
-(use-package company-ghc
-  :ensure t
-  :config
-  (add-to-list 'company-backends
-    '(company-ghc :with company-dabbrev-code))
-  (custom-set-variables '(company-ghc-show-info t)))
-
 (provide 'code/haskell)
